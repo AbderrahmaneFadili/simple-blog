42ad494da6dd5a285a1638e4459c53d11a6dc28f
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2022-07-28 16:37:02 +0100

    Blog Page Fixing

--- client/src/pages/Blog/Blog.tsx
+++ client/src/pages/Blog/Blog.tsx
@@ -1,18 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
 import Posts from "../../components/Posts/Posts";
 import { useGetPostsQuery } from "../posts.slice";
 import Spinner from "react-bootstrap/Spinner";
-import _ from "lodash";
 
 const Blog = () => {
   const { isFetching, data } = useGetPostsQuery();
 
-  useEffect(() => {
-    console.log("Blog mounted");
-  }, []);
-
-  console.log(data);
   return (
     <Container>
       <h1 className="display-1 my-3">All posts</h1>
@@ -31,7 +25,7 @@ const Blog = () => {
           />
         </div>
       )}
-      {data && <Posts posts={_.orderBy(data, ["id"], ["desc"])} />}
+      {data && <Posts posts={data} />}
     </Container>
   );
 };
